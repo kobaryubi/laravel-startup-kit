cd863a856d4aa6e3c62a65a2f70cca78f57d1293
koba <koba@growal.com>
2022-12-29 23:59:58 +0900

    feat: textarea

--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -1,6 +1,10 @@
 <x-layout>
     <form method="POST" action="{{ route('chirps.store') }}">
         @csrf
+        <textarea
+            name="message"
+            placeholder="What's on your mind?"
+        >{{ old('message') }}</textarea>
         <button type="submit">submit</button>
     </form>
 </x-layout>
