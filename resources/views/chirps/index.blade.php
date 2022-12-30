28817c84481b085cc98fc7a05022bf84ac48f535
koba <koba@growal.com>
2022-12-30 09:34:41 +0900

    feat: errors

--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -5,6 +5,7 @@
             name="message"
             placeholder="What's on your mind?"
         >{{ old('message') }}</textarea>
+        <x-input-error :messages="$errors->get('message')"/>
         <button type="submit">submit</button>
     </form>
 </x-layout>
