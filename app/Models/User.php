60895333548b9eaab11eda8ae96ded140da70aa4
koba <koba@growal.com>
2022-12-30 23:04:11 +0900

    fix: space

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ class User extends Model
      *
      * @var array<int, string>
      */
-    protected $fillable =[
+    protected $fillable = [
         'name',
         'email',
         'password',
