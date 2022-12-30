a494eb04f389c2556c0cf11e93d90781b640471e
koba <koba@growal.com>
2022-12-30 16:07:41 +0900

    feat: list

--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -8,4 +8,11 @@
         <x-input-error :messages="$errors->get('message')"/>
         <button type="submit">submit</button>
     </form>
+    <ul>
+        @foreach ($chirps as $chirp)
+            <li>
+                <p>{{ $chirp->message }}</p>
+            </li>
+        @endforeach
+    </ul>
 </x-app-layout>
