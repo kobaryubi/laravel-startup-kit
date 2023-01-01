46c8fc523644b1407f8e266ff1adba18b34b5f6f
koba <koba@growal.com>
2023-01-01 11:27:23 +0900

    feat: login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ChirpController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
@@ -37,3 +38,7 @@ Route::get('/dashboard', function() {
 Route::middleware('auth')->group(function () {
     Route::get('profile', [ProfileController::class, 'edit'])->name('profile.edit');
 });
+
+Route::get('/login', [LoginController::class, 'create'])
+    ->name('login');
+Route::post('/login', [LoginController::class, 'store']);
